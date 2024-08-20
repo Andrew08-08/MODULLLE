c88dc59db1dfb42d4bef9f4f7312de739dbeebe7
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-08-21 00:23:53 +0300

    new

--- module_1_4.py
+++ module_1_4.py
@@ -1,1 +1,7 @@
-print('hello')
+my_string = input('Да здраствует король ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(__old=' ', __new=''))
+print(my_string[0])
+print(my_string[-1])
