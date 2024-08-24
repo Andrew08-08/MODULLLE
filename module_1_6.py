d27107c9284c07e83695568c96e806218e51b231
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-08-24 23:52:06 +0300

    new

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Andrew': 1111}
+print(my_dict)
+print(my_dict['Andrew'], 2222)
+my_dict['Danzel'] = 3333
+my_dict['Freazer'] = 4444
+print(my_dict)
+print(my_dict.pop('Danzel'))
+print(my_dict)
+my_set = {666, 666, 555, 'Give', True, 555, (1, 2, 3, 4), (1, 2, 3, 4)}
+print(my_set)
+my_set.add(888)
+my_set.add(999)
+print(my_set)
