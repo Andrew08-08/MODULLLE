c2bab8f798b854d1838dd99b7afc40290f2aba5e
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-08-25 14:54:05 +0300

    new

---
+++ module_2_2.py
@@ -0,0 +1,18 @@
+first = int(input('Введите три числа: '))
+second = int(input('Введите три числа: '))
+third = int(input('Введите три числа: '))
+if (first == second == third):
+    print(3)
+elif (first == second or second == third or first == third):
+    print(2)
+else:
+    print(0)
+first = int(input('Введите три числа: '))
+second = int(input('Введите три числа: '))
+third = int(input('Введите три числа: '))
+if (first == second == third):
+    print(3)
+elif (first == second or second == third or first == third):
+    print(2)
+else:
+    print(0)
