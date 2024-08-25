29f5027838d104e96356c5a61927c203751fcec3
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-08-25 14:56:59 +0300

    new

--- module_2_2.py
+++ module_2_2.py
@@ -1,15 +1,15 @@
-first = int(input('Введите три числа: '))
-second = int(input('Введите три числа: '))
-third = int(input('Введите три числа: '))
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
 if (first == second == third):
     print(3)
 elif (first == second or second == third or first == third):
     print(2)
 else:
     print(0)
-first = int(input('Введите три числа: '))
-second = int(input('Введите три числа: '))
-third = int(input('Введите три числа: '))
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
 if (first == second == third):
     print(3)
 elif (first == second or second == third or first == third):
