50d863bdbaa3dfbaa63f000936abdae759a367af
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-12-15 21:33:39 +0300

    new

--- module_5_2.py
+++ module_5_2.py
@@ -3,29 +3,41 @@ class house:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __eq__(self, other):
-        return self.number_of_floors == other
-
-    def __lt__(self, other):
-        return self.number_of_floors < other
-
-    def __le__(self, other):
-        return self.number_of_floors <= other
-
-    def __gt__(self, other):
-        return self.number_of_floors > other
-
-    def __ge__(self, other):
-        return self.number_of_floors >= other
-
-    def __ne__(self, other):
-        return self.number_of_floors != other
-
     def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
         return f"Название: {self.name}, Кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors = (int(self.number_of_floors) + value)
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
 
     def go_to(self, new_floor):
         if 1 <= new_floor <= self.number_of_floors:
@@ -34,27 +46,26 @@ class house:
         else:
             print("Такого этажа не существует")
 
-
 h1 = house('ЖК Эльбрус', 10)
 h2 = house('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(h1 == h2)  # __eq__
+print(h1 == h2) # __eq__
 
-h1 = h1 + 10  # __add__
+h1 = h1 + 10 # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10  # __iadd__
+h1 += 10 # __iadd__
 print(h1)
 
-h2 = 10 + h2  # __radd__
+h2 = 10 + h2 # __radd__
 print(h2)
 
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
