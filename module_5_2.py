62b67bcf14969e5276db523b3f2556c07a514b08
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-12-15 20:55:39 +0300

    new

--- module_5_2.py
+++ module_5_2.py
@@ -3,20 +3,58 @@ class house:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
     def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
         return f"Название: {self.name}, Кол-во этажей: {self.number_of_floors}"
 
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
 
 h1 = house('ЖК Эльбрус', 10)
 h2 = house('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2)  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
