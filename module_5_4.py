e46da8489dff0710204e825538fc2acf9117a810
Басан Андреевич <sandzhiev.aa@bk.ru>
2024-12-22 19:37:37 +0300

    new

--- module_5_4.py
+++ module_5_4.py
@@ -1,70 +1,27 @@
 class house:
-    houses_history = []
+    house_history = []
 
     def __new__(cls, *args, **kwargs):
-        cls.houses_history = (args[0])
-        return object.__new__(cls)
+        isinstance = super().__new__(cls)
+        cls.house_history.append(args[0])
+        return isinstance
 
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, floors):
         self.name = name
-        self.number_of_floors = number_of_floors
-
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return f"Название: {self.name}, Кол-во этажей: {self.number_of_floors}"
-
-    def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
-
-    def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-
-    def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
-
-    def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
-
-    def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
-
-    def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
-
-    def __add__(self, value):
-        self.number_of_floors = (int(self.number_of_floors) + value)
-        return self
-
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def __radd__(self, value):
-        self.number_of_floors = self.number_of_floors + value
-        return self
-
-    def go_to(self, new_floor):
-        if 1 <= new_floor <= self.number_of_floors:
-            for i in range(1, new_floor + 1):
-                print(i)
-        else:
-            print("Такого этажа не существует")
+        self.floors = floors
 
     def __del__(self):
-        print(f"{self.name} снесен, но останется в истории")
-
+         print(f"{self.name} снесен, но останется в истории")
 
 h1 = house('ЖК Эльбрус', 10)
-print(house.houses_history)
+print(house.house_history)
 h2 = house('ЖК Акация', 20)
-print(house.houses_history)
+print(house.house_history)
 h3 = house('ЖК Матрёшки', 20)
-print(house.houses_history)
+print(house.house_history)
 
 # Удаление объектов
 del h2
 del h3
 
-print(house.houses_history)
+print(house.house_history)
