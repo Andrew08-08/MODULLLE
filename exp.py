973f7ca8f15bc03e211cabe754689cf5b4ef2be6
Басан Андреевич <sandzhiev.aa@bk.ru>
2025-01-05 13:49:45 +0300

    new

--- exp.py
+++ exp.py
@@ -1,55 +1,38 @@
-class SoyuzDocking:
+class Database:
     def __init__(self):
-        self.distance = 500  # Расстояние до "Салют 7" в метрах
-        self.speed = 50  # Скорость к "Салют 7" в м/с
-        self.fuel = 100  # Количество топлива
+        self.data = {}
 
-    # Сжечь топливо для замедления корабля
-    def perform_burn(self, burn_amount):
-        self.speed = max(self.speed - burn_amount, 0)
-        self.fuel = max(self.fuel - burn_amount, 0)
-
-    # Обновить расстояние на основании текущей скорости
-    def update_distance(self):
-        self.distance = max(self.distance - self.speed, 0)
-
-    # Проверить, состыковался-ли корабль к Салют-7
-    def has_docked(self):
-        return self.distance <= 0
+    def add_user(self, username, password):
+        self.data[username] = password
 
 
-# Создать последовательность стыковки
-docking_sequence = SoyuzDocking()
-# Показать инструкции к игре
-print("Добро пожаловать в симуляцию стыковки Союз Т-6!")
-print("Ваша миссия – стыковка со станцией Салют-7.")
-print("Вы можете управлять скоростью космического корабля сжигая топливо.")
-print("Каждая единица сожженного топлива замедляет космический корабль на 1 м/с.")
-print("Удачи экипажу!\n")
-# Главный игровой цикл
-while not docking_sequence.has_docked():
-    print(f"Расстояние до Салют-7: {docking_sequence.distance}метров")
-    print(f"Скорость: {docking_sequence.speed} м/с")
-    print(f"Топливо: {docking_sequence.fuel} кг")
-    # Сообщение о провале миссии в случае если закончилось топливо
-    if docking_sequence.fuel <= 0:
-        print("Кончилось топливо!")
-    break
-    # Запрос на активацию автопилота если расстояние до станции менее 11 м
-    if docking_sequence.distance < 11:
-        autopilot = input(
-            "До станции Салют-7 осталось менее 11 метров. Активировать режим автопилота для автоматической стыковки? (да/нет): ")
-    if autopilot.lower() == 'да':
-            print("Автопилот активирован.")
-    break
-    # Запрос и ввод количества топлива, которое нужно сжечь
-    burn_amount = input("Сколько сжечь топлива для снижения скорости: ")
-    burn_amount = int(burn_amount)
-    # Сжигание топлива и обновление расстояния до космической станции
-    docking_sequence.perform_burn(burn_amount)
-    docking_sequence.update_distance()
-    # Завершение процесса стыковки – проверить условия и вывести результат
-    if docking_sequence.distance <= 11 and docking_sequence.speed <= docking_sequence.distance:
-        print("Стыковка подтверждена. Поздравляем экипаж!")
-    else:
-        print("Миссия провалена. Союз Т-6 не смог состыковаться с Салют-7.")
+class User:
+    """
+    Класс пользователя, содержащий логин и пароль
+    """
+
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+
+if __name__ == '__main__':
+    database = Database()
+    while True:
+        choice = int(input("Приветствую! Выберите действие: \n1 - Enter\n2 - Registration\n"))
+        if choice == 1:
+            login = input("Введите логин: ")
+            password = input("Введите пароль: ")
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен, {login}')
+            else:
+                print('Пользователь не найден')
+        if choice == 2:
+            user = User(input("Введите логин: "), password := input("Введите пароль: "),
+                        password2 := input("Подтвердите пароль"))
+            if password != password2:
+                exit()
+            database.add_user(user.username, user.password)
+        print(database.data)
